4dc7f60a3f21ae85ed8d8c25d0861742047eabce
Amanda Rincon <anrincon@ufl.edu>
2016-03-23 14:04:30 -0700

    adjust font size of date metadata

--- single.php
+++ single.php
@@ -23,7 +23,7 @@
 				while ( have_posts() ) {
 					the_post();?>
 					<h3><?php the_title(); ?></h3>
-					<h4><?php the_date(); ?></h4>
+					<h5><?php the_date(); ?></h5>
 
 					<?php the_content();
 				}
