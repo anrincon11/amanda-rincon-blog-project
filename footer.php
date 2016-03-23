99790021c7370b2d5b5e6c30dcdcd2660b486fca
Amanda Rincon <anrincon@ufl.edu>
2016-03-23 13:03:05 -0700

    added copyright text to footer

--- footer.php
+++ footer.php
@@ -1,14 +1,9 @@
 <!-- BEGIN FOOTER -->
 	<footer class="row">
-		<div class="four columns">
-			<?php dynamic_sidebar('footer-left'); ?>
-		</div>
-		<div class="four columns">
+		<div class="twelve columns">
 			<?php dynamic_sidebar('footer-middle'); ?>
 		</div>
-		<div class="four columns">
-			<?php dynamic_sidebar('footer-right'); ?>
-		</div>
+
 	</footer>
 </div>
 
