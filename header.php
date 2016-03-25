9ccb986620fee59a564ad9f0a86c6dbc5917df91
Amanda Rincon <anrincon@ufl.edu>
2016-03-25 12:06:36 -0700

    style custom search bar to match mockup

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
     <title><?php bloginfo('name'); ?></title>
 <?php wp_head(); ?>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css" />
 
 </head>
 <body>
